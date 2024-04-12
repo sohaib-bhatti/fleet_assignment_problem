26c83ad2219edc1912087aaad7bd2050fdf4dda8
Sohaib Bhatti <44452143+sohaib-bhatti@users.noreply.github.com>
2024-04-11 21:53:45 -0400

    added constraint for passenger demand

--- MILP.py
+++ MILP.py
@@ -1,8 +1,8 @@
-import scipy
 import numpy as np
-import scipy.optimize
+import scipy.optimize as opt
 
 
+# use this to ensure x is a col. vector
 def flatten(U, T):
     E = U.transpose() @ T
     # print("the big ol matrix is of shape", np.shape(E))
@@ -17,27 +17,32 @@ def main():
     num_aircraft_types = np.size(U)
     num_destinations = np.size(T)
 
-    x_size = np.size(num_aircraft_types * num_destinations)
+    x_size = num_aircraft_types * num_destinations
 
     c = flatten(U, T)
     # print("c is of shape", np.shape(c))
     # first 10 entries of c correspond to the first airplane type,
     # next 10 to the second airplane type, etc.
 
-    demand = np.array([[700, 300, 200, 700, 100, 900, 1000, 300, 1300, 1000]])
-    capacities = np.array([[100, 200, 300, 400, 500]])
+    demand = np.array([700, 300, 200, 700, 100, 900, 1000, 300, 1300, 1000])
+    capacities = np.array([100, 200, 300, 400, 500])
 
-    x_star = scipy.optimize.milp(c)
-    
-    A = np.zeros([x_size, num_destinations])
+    A = np.zeros([num_destinations, x_size])
 
     # ensure that flights going to each airport meet passenger demands
 
-    for i in range(num_aircraft_types):
-        for j in range(num_destinations):
-            
+    for i in range(num_destinations):
+        for j in range(num_aircraft_types):
+            A[i, j * 10 + i] = capacities[j]
 
-    # print(x_star)
+    constraints = opt.LinearConstraint(A, lb=demand)
+
+    """""""""""""""""""""
+    THE STAR OF THE SHOW
+    """""""""""""""""""""
+    x_star = opt.milp(c, constraints=constraints)
+
+    print(x_star)
 
 
 if __name__ == "__main__":
